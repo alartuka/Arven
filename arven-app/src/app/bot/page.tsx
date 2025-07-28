98222df3961cff3a7f5ee1425453c272507fc807
alartuka <tukaalarbi@gmail.com>
2025-07-28 11:23:55 -0400

    resetting project for vercel

---
+++ arven-app/src/app/bot/page.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import ChatBot from '@/components/ChatBot';
+
+
+export default function Bot() {
+  return (
+    <div className="flex flex-col h-screen max-w-8xl mx-auto from-blue-50 to-indigo-100">
+      <ChatBot />
+    </div>
+  )
+}
